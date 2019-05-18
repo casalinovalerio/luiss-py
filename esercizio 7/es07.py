6161f8a3047f150d50a65725dacccdcd9d108006
Valerio Casalino <casalinovalerio.cv@gmail.com>
2019-05-18 20:09:45 +0200

    Esercizio 7 aggiunto

--- esercizio 7/es07.py
+++ esercizio 7/es07.py
@@ -1,10 +1,3 @@
-# Esercizio 7
-
-![es 7](testo-es7.assets/esercizio7.jpeg)
-
-# Soluzione nel file
-
-```python
 s1 = raw_input("Inserire la stringa 1: ")
 s2 = raw_input("\nInserire la stringa 2: ")
 x  = int(raw_input("\nInserire il numero: "))
@@ -15,5 +8,3 @@ if x < l1:
 	print s1[0 : x] + s2 + s1[x+1 : l1]
 else:
 	print "\nerrore: la lunghezza della prima stringa è: " + str(l1)
-```
-
